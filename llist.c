98fd0d1ce413e3b037523c0d72077367cf438c70
drezzyts <drezzydev@gmail.com>
2024-07-13 00:04:39 -0300

    feat: implementing some crud functions.

--- llist.c
+++ llist.c
@@ -72,4 +72,112 @@ node_t* node_last(const node_t* node) {
     last = last->next;
 
   return last;
+}
+
+node_t* node_at(const node_t* node, int index) {
+  node_t* current = index < 0 ? node_last(node) : node_first(node);
+
+  if (current == NULL) return NULL;
+
+  if (index > 0)
+    return node_next(current, index);
+  else if (index < 0)
+    return node_previous(current, abs(index + 1));
+  else
+    return current;
+}
+
+node_t *node_find(const node_t* node, const void *expected, 
+  bool (*compare_function)(const void *expected, const void *current))
+{
+  node_t* current = node_first(node);
+  
+  while(current != NULL) {
+    bool is_equal = compare_function(expected, current->value);
+    if (is_equal) return current;
+    current = current->next;
+  }
+
+  return NULL;
+};
+
+node_t* node_next(const node_t* node, size_t offset) {
+  node_t* current = node->next;
+
+  for (int i = 0; i < offset - 1; i++) {
+    if (current == NULL || current->next == NULL) 
+      return NULL;
+
+    current = current->next; 
+  };
+
+  return current;
+};
+
+node_t* node_previous(const node_t* node, size_t offset) {
+  node_t* current = node->previous;
+
+  for (int i = 0; i < offset - 1; i++) {
+    if (current == NULL || current->next == NULL) 
+      return NULL;
+
+    current = current->previous; 
+  };
+
+  return current;
+};
+
+size_t node_size(const node_t* node) {
+  node_t* current = node_first(node);
+  size_t size = 0;
+
+  while (current != NULL) {
+    current = current->next;
+    size++;
+  }
+
+  return size;
+};
+
+bool node_empty(const node_t* node) {
+  return node->next == NULL && node->previous == NULL ? true : false;
+};
+
+void node_delete(node_t* node) {
+  node_t* next_node = node->next;
+  node_t* prev_node = node->previous;
+
+  if (next_node)
+    next_node->previous = prev_node;
+  
+  if (prev_node)
+    prev_node->next = next_node;
+
+  free(node);
+}
+
+void node_remove(node_t* node, const void *expected, 
+  bool (*compare_function)(const void *expected, const void *current)) 
+{
+  node_t* current = node_first(node);
+
+  while(current != NULL) {
+    bool is_equal = compare_function(expected, current->value);
+    if (is_equal) return node_delete(current);
+    current = current->next;
+  }
+};
+
+void node_pop(node_t* node) {
+  node_t* last = node_last(node);
+  if (last == NULL) return;
+
+  node_delete(last);
+};
+
+void node_shift(node_t* node) {
+  node_t* first = node_first(node);
+  if (first == NULL) return;
+
+  node_delete(first); 
 };
--- llist.h
+++ llist.h
@@ -1,6 +1,32 @@
 #ifndef LLIST_H
 #define LLIST_H
 
+#ifndef NODE_INT_COMPARE
+#define NODE_INT_COMPARE __node_int_cmp
+#endif
+
+#ifndef NODE_FLOAT_COMPARE
+#define NODE_FLOAT_COMPARE __node_float_cmp
+#endif
+
+#ifndef NODE_STRING_COMPARE
+#define NODE_STRING_COMPARE __node_string_cmp
+#endif
+
+#ifndef NODE_CHAR_COMPARE
+#define NODE_CHAR_COMPARE __node_char_cmp
+#endif
+
+#ifndef __NODE_EPSILON
+#define __NODE_EPSILON 1e-5
+#endif
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
 typedef struct Node {
   struct Node* next;
   struct Node* previous;
@@ -8,11 +34,47 @@ typedef struct Node {
 } node_t;
 
 node_t* node_create(void*);
+
 node_t* node_append(node_t*, void*);
 node_t* node_prepend(node_t*, void*);
 node_t* node_pushback(node_t*, void*);
 node_t* node_pushfront(node_t*, void*);
+
 node_t* node_first(const node_t*);
 node_t* node_last(const node_t*);
+node_t* node_next(const node_t*, size_t);
+node_t* node_previous(const node_t*, size_t);
+node_t* node_at(const node_t*, int);
+node_t* node_find(const node_t*, const void* expected,
+  bool (*compare_function)(const void* expected, const void* current));
+
+void node_delete(node_t*);
+void node_remove(node_t*, const void* expected, 
+  bool (*compare_function)(const void* expected, const void* current));
+void node_pop(node_t*);
+void node_shift(node_t*);
+
+size_t node_size(const node_t*);
+bool node_empty(const node_t*);
+
+bool __node_int_cmp(const void* expected, const void* current) {
+  if (expected == NULL || current == NULL) return false;
+  return (*(int*) expected) == (*(int*) current) ? true : false;
+}
+
+bool __node_float_cmp(const void* expected, const void* current) {
+  if (expected == NULL || current == NULL) return false;
+  return fabs(*(float*) expected - *(float*) current) < __NODE_EPSILON;
+}
+
+bool __node_string_cmp(const void* expected, const void* current) {
+  if (expected == NULL || current == NULL) return false;
+  return strcmp(*(char**) expected, *(char**) current) == 0 ? true : false;
+}
+
+bool __node_char_cmp(const void* expected, const void* current) {
+  if (expected == NULL || current == NULL) return false;
+  return (*(char*) expected) == (*(char*) current) ? true : false;
+}
 
 #endif
